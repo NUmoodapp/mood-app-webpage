dbfa7478447490a133814ceee0626a99aa28ef14
JRHecimovich2242 <jrhecimovich@icloud.com>
2022-02-11 15:20:28 -0600

    IBM functional with webpage
    Pushing to test on a separate machine to confirm functionality

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 import time
 from flask import Flask, request
+from IBMWatson import SentimentAnalysis
 import json
 import urllib.request
 import re
@@ -11,7 +12,7 @@ def get_song():
     statement = request.get_json().get('statement')
     # right now, statement will hold exactly what Azure transcribed
     # i.e., if Watson does text cleaning, it should be implemented here
-
+    SentimentAnalysis(statement)
     # right now, I just search youtube for the statement and return the id of the first video
     search_keywords = "+".join(statement.split())
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keywords);
@@ -19,3 +20,5 @@ def get_song():
     if not video_ids:
         return {'song': ''}
     return {'song': [statement, "https://www.youtube.com/embed/" + video_ids[0]]}
+
+
