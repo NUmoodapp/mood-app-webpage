3b9a673a19bbfb2ef4ac3bc3c569930fabc9670e
aconeeny9 <75339193+aconeeny9@users.noreply.github.com>
2022-02-15 16:18:58 -0600

    Adds Spotify token grant flow

--- src/token_util.js
+++ src/token_util.js
@@ -24,3 +24,25 @@ export async function getTokenOrRefresh() {
         return { authToken: speechToken.slice(idx + 1), region: speechToken.slice(0, idx) };
     }
 }
+
+export async function getSpotifyTokenOrRefresh() {
+    const cookie = new Cookie();
+    const spotifyToken = cookie.get('spotify-token');
+
+    if (spotifyToken === undefined) {
+        try {
+            const res = await axios.get('http://localhost:3001/api/get-spotify-token');
+            const token = res.data.token.access_token;
+            cookie.set('spotify-token', token, {maxAge: 540, path: '/'});
+
+            console.log('Spotify token fetched from back-end: ' + token);
+            return { authToken: token };
+        } catch (err) {
+            console.log("error in getSpotifyTokenOrRefresh: ",err.response.data);
+            return { authToken: null, error: err.response.data };
+        }
+    } else {
+        console.log('Spotify token fetched from cookie: ' + spotifyToken);
+        return { authToken: spotifyToken };
+    }
+}
