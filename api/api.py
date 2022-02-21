cf4c9fa5c80adcd1bc588b8c283c0b015e8b7741
Josh Levitas <joshlevitas@dhcp-10-105-132-51.wireless.northwestern.private>
2022-02-21 12:05:14 -0600

    made progress on combining IBM + genius, working on formatting response

--- api/api.py
+++ api/api.py
@@ -6,6 +6,7 @@ import urllib.request
 import re
 import api_keys
 import requests
+import itertools
 
 app = Flask(__name__)
 
@@ -41,8 +42,69 @@ def search_genius(search_term):
     return json_data
 
 def get_song_list(json_data): #input = output of search()
+    song_list = []
     for song in json_data["response"]["hits"]:
-        print(song["result"]["full_title"])
+        song_list.append(song["result"]["full_title"])
+    return song_list
 
 
 # Use: get_song_list(search_genius("[search term]"))
+
+def convertTuple(tup):
+    str = ''
+    for item in range(len(tup)):
+        if item == 0:
+            str = str + tup[item]
+        else: 
+            str = str + " " + tup[item]
+    return str
+
+def strip_combos(combos):
+    ret = []
+    for c in combos:
+        if c!= [()]:
+            for combo in c:
+                ret.append(convertTuple(combo))
+
+    return ret
+
+
+   
+        
+
+stop_words = ['the', 'and', 'a', 'an']
+
+def connect_genius(search_term): #rename
+    song_list = []
+    res = SentimentAnalysis(search_term)
+    search_keywords = res['keywords']
+    search_keywords_list = []
+    for s in search_keywords:
+        search_keywords_list.append(s['text'])
+    # print(search_keywords_list)
+
+   
+    combos = []
+    for r in range(len(search_keywords_list)+1):
+        combinations_obj = itertools.combinations(search_keywords_list, r)
+        combos.append(list(combinations_obj))
+
+    combos = strip_combos(combos)
+    
+
+
+    for word_combo in combos:
+        if word_combo.lower() not in stop_words: 
+            song_list.append(get_song_list(search_genius(word_combo)))
+
+    return song_list
+
+#search combinations of keywords
+
+# print(connect_genius('i am sad and i like legos and airplanes and basketball'))
+# connect_genius('i am sad and i like legos and airplanes and basketball')
+
+# combos = [[()], [('airplanes',), ('basketball',), ('legos',)], [('airplanes', 'basketball'), ('airplanes', 'legos'), ('basketball', 'legos')], [('airplanes', 'basketball', 'legos')]]
+# print(strip_combos(combos))
+
+print(get_song_list(search_genius("travel")))
