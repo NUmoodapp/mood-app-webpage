95b2bf7cd05235911942f5ceba2facfdaa4eb12b
JRHecimovich2242 <jrhecimovich@icloud.com>
2022-02-27 14:55:23 -0600

    Added MakeConceptTuples and MakeKeywordTuples
    Created two new functions to make lists of tuples for keywords and concepts where each tuple is (text, relevance).

--- api/IBMWatson.py
+++ api/IBMWatson.py
@@ -1,3 +1,4 @@
+from cgi import test
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -25,7 +26,7 @@ def SentimentAnalysis(statement):
                 relations=RelationsOptions(),
                 entities=EntitiesOptions(),
             )).get_result()
-        #print(json.dumps(response, indent=2))
+        print(json.dumps(response, indent=2))
         result = json.loads(json.dumps(response, indent=2))
         return result
     except:
@@ -35,8 +36,25 @@ def SentimentAnalysis(statement):
 # exampleText = "Ah, Bikini Bottom. Home to the friendliest creatures under the sea. But how do you know if you're really from Bikini Bottom? You know you are from Bikini Bottom when... this is how you drive."
 # example = SentimentAnalysis(exampleText)
 # print(example['emotion']['document']['emotion'])#
-# exampleText = "Jane just broke up with me. I really thought she was the one and I dont know what I will do without her. I should have treated her better. I miss when she was my girlfriend. I loved her."
-#example = SentimentAnalysis(exampleText)
+exampleText = "Jane just broke up with me. I really thought she was the one and I dont know what I will do without her. I should have treated her better. I miss when she was my girlfriend. I loved her."
+example = SentimentAnalysis(exampleText)
 # exampleText = "Who lives in a pineapple under the sea? SpongeBob SquarePants! Absorbent and yellow and porous is he SpongeBob SquarePants! If nautical nonsense be something you wish SpongeBob SquarePants! Then drop on the deck and flop like a fish! SpongeBob SquarePants!"
 # example = SentimentAnalysis(exampleText)
 #print(example)
+
+def MakeConceptTuples(response):
+    conceptList = []
+    for concept in response['concepts']:
+        conceptList.append((concept['text'], concept['relevance']))
+    return conceptList
+
+def MakeKeywordTuples(response):
+    keywordList = []
+    for keyword in response['keywords']:
+        keywordList.append((keyword['text'], keyword['relevance']))
+    return keywordList
+
+# testList = MakeConceptTuples(example)
+# print(testList)
+# testList = MakeKeywordTuples(example)
+# print(type(testList[1][1]))
