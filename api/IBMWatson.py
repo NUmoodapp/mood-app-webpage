0e5bded1d5e172b2a681620d1b8b313b177849fe
JRHecimovich2242 <jrhecimovich@icloud.com>
2022-02-27 14:56:23 -0600

    Cleaned up test cases

--- api/IBMWatson.py
+++ api/IBMWatson.py
@@ -36,8 +36,8 @@ def SentimentAnalysis(statement):
 # exampleText = "Ah, Bikini Bottom. Home to the friendliest creatures under the sea. But how do you know if you're really from Bikini Bottom? You know you are from Bikini Bottom when... this is how you drive."
 # example = SentimentAnalysis(exampleText)
 # print(example['emotion']['document']['emotion'])#
-exampleText = "Jane just broke up with me. I really thought she was the one and I dont know what I will do without her. I should have treated her better. I miss when she was my girlfriend. I loved her."
-example = SentimentAnalysis(exampleText)
+# exampleText = "Jane just broke up with me. I really thought she was the one and I dont know what I will do without her. I should have treated her better. I miss when she was my girlfriend. I loved her."
+# example = SentimentAnalysis(exampleText)
 # exampleText = "Who lives in a pineapple under the sea? SpongeBob SquarePants! Absorbent and yellow and porous is he SpongeBob SquarePants! If nautical nonsense be something you wish SpongeBob SquarePants! Then drop on the deck and flop like a fish! SpongeBob SquarePants!"
 # example = SentimentAnalysis(exampleText)
 #print(example)
